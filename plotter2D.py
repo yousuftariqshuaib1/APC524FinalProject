e5be0498f7f1a83bc5f4cc729614a9de7a75f600
Yousuf Tariq-Shuaib <symit@princeton.edu>
2022-12-15 22:44:52 -0500

    Changed angle to be relative to velocity

--- plotter2D.py
+++ plotter2D.py
@@ -10,7 +10,7 @@ def GPS_plotter(state: np.array, plane, background):
     x_pos = int(width_b / 2 + state[3] / 20 - width_p/2)
     y_pos = int(height_b / 2 - state[4] / 20 - width_p/2)
     
-    plane_rotated = plane.rotate(state[11]-90 expand = 1)
+    plane_rotated = plane.rotate(state[11]-90, expand = 1)
     background_temp.paste(plane_rotated, (x_pos,y_pos), plane_rotated)
     
     return background_temp
