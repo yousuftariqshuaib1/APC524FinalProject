7a834e491c3851fcefe77c48d245847ace9e8c7e
Yousuf Tariq-Shuaib <symit@princeton.edu>
2022-12-15 22:24:13 -0500

    Added a bunch of stuff

--- plotter2D.py
+++ plotter2D.py
@@ -1,29 +1,15 @@
 
 import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib.offsetbox import OffsetImage, AnnotationBbox
-from matplotlib.cbook import get_sample_data
-import cv2
+from PIL import Image
 
-def GPS_Plotter(state: np.array, image):
-    fig, ax = plt.subplots()
-    height, width = image.shape[:2]
-    center = (width/2, height/2)
+def GPS_plotter(state: np.array, plane, background):
+    background_temp = background.copy()
+    width_b, height_b = background_temp.size
+    width_p, height_p = plane.size
     
-    rotate_matrix = cv2.getRotationMatrix2D(center=center, angle=-state[11], scale=1)
-    rotated_image = cv2.warpAffine(src=image, M=rotate_matrix, dsize=(width, height))
+    x_pos = int(width_b / 2 + state[3] / 10 - width_p/2)
+    y_pos = int(height_b / 2 - state[4] / 10 - width_p/2)
+    plane_rotated = plane.rotate(state[11], expand = 1)
+    background_temp.paste(plane_rotated, (x_pos,y_pos), plane_rotated)
     
-    im = OffsetImage(rotated_image, zoom=0.05)
-    
-    ab = AnnotationBbox(im, (state[3], state[4]), xycoords='data', frameon=False)
-    ax.add_artist(ab)
-    ax.set_xlim(-5000.0, 5000.0)
-    ax.set_ylim(-5000.0, 5000.0)
-    title = "Altitude: "
-    title = title + str(state[5])
-    plt.title(title)
-    
-    plt.savefig('images/GPSPlot.jpg')
-    plt.close(fig)
-    
-    
+    return background_temp
--- plotter3D.py
+++ plotter3D.py
@@ -1,4 +1,3 @@
-
 import numpy as np
 from mpl_toolkits.mplot3d import axes3d
 from matplotlib.colors import Normalize
@@ -6,9 +5,7 @@ import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 import matplotlib
 
-def vector_3D_plotter(state: np.array):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
+def vector_3D_plotter(state: np.array, fig):
     
     colors = np.array([0, 0.5, 1])
     norm = Normalize()
@@ -24,12 +21,12 @@ def vector_3D_plotter(state: np.array):
     
     colormap = cm.inferno
     
-    ax.quiver(x, y, z, u, v, w, color=colormap(norm(colors)), length=4000, normalize=True)
-    ax.set_xlim(-5000.0, 5000.0)
-    ax.set_ylim(-5000.0, 5000.0)
-    ax.set_zlim(0, 15000.0)
+    ax3D = fig.add_subplot(projection='3d')
+    ax3D.quiver(x, y, z, u, v, w, color=colormap(norm(colors)), length=4000, normalize=True)
+    ax3D.set_xlim(-5000.0, 5000.0)
+    ax3D.set_ylim(-5000.0, 5000.0)
+    ax3D.set_zlim(0, 15000.0)
     
     plt.savefig('images/vector3DPlot.jpg')
-    plt.close(fig)
-    
+    plt.clf()
     
