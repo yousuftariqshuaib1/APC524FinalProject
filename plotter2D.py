2482cb3c64dfee57393d9ac1b9447de8343db245
Yousuf Tariq-Shuaib <symit@princeton.edu>
2022-12-15 22:31:35 -0500

    Added more crap

--- plotter2D.py
+++ plotter2D.py
@@ -7,8 +7,8 @@ def GPS_plotter(state: np.array, plane, background):
     width_b, height_b = background_temp.size
     width_p, height_p = plane.size
     
-    x_pos = int(width_b / 2 + state[3] / 10 - width_p/2)
-    y_pos = int(height_b / 2 - state[4] / 10 - width_p/2)
+    x_pos = int(width_b / 2 + state[3] / 20 - width_p/2)
+    y_pos = int(height_b / 2 - state[4] / 20 - width_p/2)
     plane_rotated = plane.rotate(state[11], expand = 1)
     background_temp.paste(plane_rotated, (x_pos,y_pos), plane_rotated)
     
